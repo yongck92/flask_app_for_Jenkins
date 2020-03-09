dabecebad6f47d81763315beeafcd142e58a5595
Michael Mortenson (Warwick) <48418248+MJMortensonWarwick@users.noreply.github.com>
2020-03-09 21:19:00 +0000

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-From flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world:
-    return 'A Python app in a Docker container!'
-
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
