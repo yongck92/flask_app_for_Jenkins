2d6c0afc010732cc441526273d59f7df15936fa1
Michael Mortenson (Warwick) <48418248+MJMortensonWarwick@users.noreply.github.com>
2020-03-09 21:19:13 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world:
+	df = pd.read_csv("Names.csv")
+	name = df.query('ID==' + str(random.randint(1,4)))['Name'].to_string(index=False)
+	return "Hello, " + name + "!"
+
+if __name == '__main__':
+    app.run(debug=True, host='0.0.0.0')
