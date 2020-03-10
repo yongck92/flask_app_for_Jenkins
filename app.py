eb044d6696234b5e8e1d8ae866ed7b6dfef8e148
Michael Mortenson (Warwick) <48418248+MJMortensonWarwick@users.noreply.github.com>
2020-03-10 12:10:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	df = pd.read_csv("Names.csv")
-	name = df.query('ID==' + str(random.randint(1,4)))['Name'].to_string(index=False)
+	names = {1: "Mark", 2: "Liping", 3: "Jordan", 4: "Michael"
+	name = names[random.randint(1,4)]
 	return "Hello, " + name + "!"
 
 if __name__ == '__main__':
