172622dd49eadbe665116edda3a2558fb61e482d
Michael Mortenson (Warwick) <48418248+MJMortensonWarwick@users.noreply.github.com>
2020-03-10 12:21:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	names = {1: "Mark", 2: "Liping", 3: "Jordan", 4: "Michael"
+	names = {1: "Mark", 2: "Liping", 3: "Jordan", 4: "Michael"}
 	name = names[random.randint(1,4)]
 	return "Hello, " + name + "!"
 
