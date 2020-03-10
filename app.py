b11f8295062ca900edcb6e1928a6b9d614263b90
Michael Mortenson (Warwick) <48418248+MJMortensonWarwick@users.noreply.github.com>
2020-03-10 15:57:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
 	names = {1: "Mark", 2: "Liping", 3: "Jordan", 4: "Michael"}
 	name = names[random.randint(1,4)]
-	return "Hello, " + name + "!"
+	return "Hello, " + name + "!!!!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
